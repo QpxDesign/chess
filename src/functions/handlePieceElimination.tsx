97edc00c78736b366c30967578dc3d1333b147d2
Quinn Patwardhan <quinnpatwardhan@quinns-mbp.mynetworksettings.com>
2023-02-13 19:22:16 -0500

    changed url

--- src/functions/handlePieceElimination.tsx
+++ src/functions/handlePieceElimination.tsx
@@ -39,7 +39,7 @@ export function handlePieceElimination(
         moveData: JSON.stringify(mo),
       };
 
-      await fetch("http://localhost:3001/handle-move", {
+      await fetch("https://chess-api.quinnpatwardhan.com/handle-move", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
